6d157154fb98dfd7c90b739568bc71955a5a7ff1
cbellom <cbellom@unal.edu.co>
2017-01-13 17:05:41 -0500

    init test

---
+++ Assets/Scripts/Player/MoveController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveController : MonoBehaviour
+{
+    [SerializeField]
+    private float jumpForce = 10f;
+    [SerializeField]
+    private Quaternion rotation;
+    [SerializeField]
+    private Vector3 radius = new Vector3(5, 0, 0);
+
+    private float currentRotation = 0.0f;
+    private Rigidbody playerRigidbody;
+
+    void Awake() {
+        playerRigidbody = GetComponent<Rigidbody>();
+        UpdateRadiusToAxisYPosition();
+    }
+
+    void Update() {
+        UpdateRadiusToAxisYPosition();
+        GetInputToMove();
+    }
+
+    private void UpdateRadiusToAxisYPosition() {
+        radius.y = transform.position.y;
+    }
+
+    private void GetInputToMove() {
+        currentRotation += Input.GetAxis("Horizontal") * Time.deltaTime * 100;
+        rotation.eulerAngles = new Vector3(0, currentRotation, 0);
+        transform.position = rotation * radius;
+
+        transform.rotation = rotation;
+    }
+
+    private void getInputToJump() {
+        if (Input.GetAxis("Vertical") > 0)
+            playerRigidbody.AddForce(0, jumpForce, 0); ;
+    }
+}
---
+++ Assets/Scripts/World/WorldRotationController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldRotationController : MonoBehaviour {
+
+    [SerializeField]
+    private float rotationSpeed;
+
+    void Update()
+    {
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        rotation *= Time.deltaTime;
+        transform.Rotate(0, rotation, 0);
+    }
+}
