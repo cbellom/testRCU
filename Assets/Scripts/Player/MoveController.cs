b5da39cdad0b1a3bface456070269fcdce8761ed
Estudiante <Estudiante@unad.edu.co>
2017-01-13 22:05:45 -0500

    asdasdasd

--- Assets/Scripts/Player/MoveController.cs
+++ Assets/Scripts/Player/MoveController.cs
@@ -4,14 +4,17 @@ using System.Collections;
 public class MoveController : MonoBehaviour
 {
     [SerializeField]
+    private float moveForce = 10f;
+    [SerializeField]
     private float jumpForce = 10f;
     [SerializeField]
-    private Quaternion rotation;
-    [SerializeField]
     private Vector3 radius = new Vector3(5, 0, 0);
-
+    
+    private bool isJumping = false;
     private float currentRotation = 0.0f;
+	private Quaternion rotation;
     private Rigidbody playerRigidbody;
+    private Animator playerAnimator;
 
     void Awake() {
         playerRigidbody = GetComponent<Rigidbody>();
@@ -19,24 +22,31 @@ public class MoveController : MonoBehaviour
     }
 
     void Update() {
-        UpdateRadiusToAxisYPosition();
         GetInputToMove();
     }
-
+    
     private void UpdateRadiusToAxisYPosition() {
         radius.y = transform.position.y;
     }
 
     private void GetInputToMove() {
-        currentRotation += Input.GetAxis("Horizontal") * Time.deltaTime * 100;
-        rotation.eulerAngles = new Vector3(0, currentRotation, 0);
-        transform.position = rotation * radius;
 
+        if (Input.GetAxis("Vertical") > 0)
+            transform.position += transform.up * jumpForce * Time.deltaTime;
+
+        currentRotation += Input.GetAxis("Horizontal") * Time.deltaTime * moveForce;
+        rotation.eulerAngles = new Vector3(0, -currentRotation, 0);
+        
+        transform.position = rotation * radius;
+        
+        transform.position = rotation * radius;
         transform.rotation = rotation;
     }
 
-    private void getInputToJump() {
-        if (Input.GetAxis("Vertical") > 0)
-            playerRigidbody.AddForce(0, jumpForce, 0); ;
+    private void PlayRunAnimation(float moveSpeed) {
+        if(Input.GetKeyDown("a") || Input.GetKeyDown("d")) {
+            playerAnimator.SetFloat("moveSpeed", moveSpeed);
+        }
     }
+    
 }
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 6.0F;
+    [SerializeField]
+    private float jumpSpeed = 8.0F;
+    [SerializeField]
+    private float gravity = 20.0F;
+    [SerializeField]
+    private Vector3 radius = new Vector3(5, 0, 0);
+
+    private float currentRotation = 0.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    private Quaternion rotation;
+    private CharacterController controller;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            currentRotation += Input.GetAxis("Vertical") * Time.deltaTime * speed;
+            rotation.eulerAngles = new Vector3(0, -currentRotation, 0);
+            transform.rotation = rotation;
+
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
